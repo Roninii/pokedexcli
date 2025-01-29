cc8b51bdca86bf75459cf0851bb772a4ace7597f
Roninii <ronnie@ronini.dev>
2025-01-29 14:34:15 -0600

    add map and mapb commands

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -10,7 +12,12 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
+}
+
+type Config struct {
+	Next     string
+	Previous string
 }
 
 var commands map[string]cliCommand
@@ -27,10 +34,21 @@ func init() {
 			description: "Show available commands",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Show a paginated list of map locations; subsequent calls will show the next page of results.",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Show the previous page of map locations.",
+			callback:    commandMapb,
+		},
 	}
 }
 
 func main() {
+	config := &Config{}
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -46,9 +64,9 @@ func main() {
 			continue
 		}
 
-		err := command.callback()
+		err := command.callback(config)
 		if err != nil {
-			fmt.Println("Error executing command:", err)
+			fmt.Printf("Error executing command: %s; %v\n", command.name, err)
 		}
 
 	}
@@ -61,20 +79,88 @@ func cleanInput(input string) []string {
 	return strings.Split(strings.ToLower(strings.TrimSpace(input)), " ")
 }
 
-func commandExit() error {
+func commandExit(config *Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *Config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Available commands:")
 	fmt.Println("")
 
 	for _, command := range commands {
 		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	return nil
+}
+
+type LocationAreaResponse struct {
+	Count    int            `json:"count"`
+	Next     string         `json:"next"`
+	Previous string         `json:"previous"`
+	Results  []LocationArea `json:"results"`
+}
+type LocationArea struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func commandMap(config *Config) error {
+	var url string
+	if config.Next != "" {
+		url = config.Next
+	} else {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("Error fetching map data: %v", err)
+	}
+
+	defer res.Body.Close()
+	var mapData LocationAreaResponse
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&mapData)
+	if err != nil {
+		return fmt.Errorf("Error decoding map data: %v", err)
+	}
+
+	config.Next = mapData.Next
+	config.Previous = mapData.Previous
+	for _, location := range mapData.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(config *Config) error {
+	if config.Previous == "" {
+		return fmt.Errorf("Already at the beginning of the map!")
+	}
+
+	res, err := http.Get(config.Previous)
+	if err != nil {
+		return fmt.Errorf("Error fetching map data: %v", err)
+	}
+
+	defer res.Body.Close()
+	var mapData LocationAreaResponse
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&mapData)
+	if err != nil {
+		return fmt.Errorf("Error decoding map data: %v", err)
+	}
+
+	config.Next = mapData.Next
+	config.Previous = mapData.Previous
+	for _, location := range mapData.Results {
+		fmt.Println(location.Name)
 	}
 
 	return nil
